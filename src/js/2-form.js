caab02f5ecdd4bd2926b08164afb8083a0d7282a
Olevihon <charleksvik@gmail.com>
2024-04-30 12:46:22 +0300

    fixes

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,34 +2,20 @@ import '../css/2-form.css';
 
 const FEEDBACK_STORAGE_KEY = 'feedback-form-state';
 
-const formRefs = {
+const refs = {
   form: document.querySelector('.feedback-form'),
-  input: document.querySelector('input[name="email"]'),
-  message: document.querySelector('textarea'),
 };
 
-const formData = {
-  email: '',
-  message: '',
-};
+const { email, message } = refs.form.elements;
 
-const storedFormData = localStorage.getItem(FEEDBACK_STORAGE_KEY);
-if (storedFormData) {
-  const formData = JSON.parse(localStorage.getItem(FEEDBACK_STORAGE_KEY));
-  formRefs.input.value = formData.email;
-  formRefs.message.value = formData.message;
+let formData = JSON.parse(localStorage.getItem(FEEDBACK_STORAGE_KEY)) || {};
+
+if (formData) {
+  email.value = formData.email || '';
+  message.value = formData.message || '';
 }
 
 const onFormInput = e => {
-  e.preventDefault();
-  const { email, message } = e.currentTarget.elements;
-
-  // Prevent pollute user localStorage
-  if (storedFormData && email.value === '' && message.value === '') {
-    localStorage.removeItem(FEEDBACK_STORAGE_KEY);
-    return;
-  }
-
   formData[e.target.name] = e.target.value.trim();
   localStorage.setItem(FEEDBACK_STORAGE_KEY, JSON.stringify(formData));
 };
@@ -37,11 +23,7 @@ const onFormInput = e => {
 const onFormSubmit = e => {
   e.preventDefault();
 
-  const form = e.currentTarget;
-  const email = form.elements.email.value;
-  const message = form.elements.message.value;
-
-  if (email === '' || message === '') {
+  if (email.value === '' || message.value === '') {
     alert('Fill please all fields');
     return;
   }
@@ -49,12 +31,9 @@ const onFormSubmit = e => {
   console.log(formData);
 
   localStorage.removeItem(FEEDBACK_STORAGE_KEY);
-  formData.email = '';
-  formData.message = '';
-  form.elements.email.value = '';
-  form.elements.message.value = '';
-  e.currentTarget.reset();
+  formData = {};
+  e.target.reset();
 };
 
-formRefs.form.addEventListener('input', onFormInput);
-formRefs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', onFormInput);
+refs.form.addEventListener('submit', onFormSubmit);
